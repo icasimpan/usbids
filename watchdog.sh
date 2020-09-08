ebcd975940afced9434533e21b96ff9a09de7d0c
Ismael Casimpan Jr <ismael.angelo@casimpan.com>
2020-09-08 20:31:37 +0800

    watchdog.sh: Make it a little-bit verbose.

--- watchdog.sh
+++ watchdog.sh
@@ -3,6 +3,7 @@
 ## Get current version.
 ##
 GITHUB_VER=$(grep Version: usb.ids|cut -d':' -f2|sed 's/\ //g')
+echo "[INFO] Current Version: ${GITHUB_VER}."
 
 ## Get upstream data + version.
 ##
@@ -12,8 +13,9 @@ UPSTREAM_VER=$(grep Version: usb.ids|cut -d':' -f2|sed 's/\ //g')
 ## Changes seen? Commit. Otherwise, say so.
 ##
 if [[ "$GITHUB_VER" = "$UPSTREAM_VER" ]]; then
-  echo "[INFO] No changes to USB ID Repository."
+  echo "[INFO] No changes to USB ID Repository (Version: $UPSTREAM_VER)."
 else
+  echo "[INFO] New version detected: ${UPSTREAM_VER}."
   git add usb.ids;
   REMOTE_REPO=$(git remote get-url --push origin|cut -d'@' -f2)
   git remote add delta https://${GH_TOKEN}@${REMOTE_REPO}
@@ -21,4 +23,14 @@ else
   git config user.email "ismael.angelo@casimpan.com"
   git commit -m"$UPSTREAM_VER changes."
   git push -u delta master
+
+  ## Make sure github commit is ok.
+  ##
+  commit_ok=$?
+  if [[ $commit_ok -eq 0 ]]; then
+    echo "[INFO] Github mirror commit ok."
+  else
+    echo "[ERROR]: Something went wrong, Github mirror commit failed"
+    exit 1
+  fi
 fi
